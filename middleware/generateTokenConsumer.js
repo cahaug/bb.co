c7fbf043b0a076a180839caf5602227b518b4bd0
Cale Haug <caleahaug@gmail.com>
2021-07-30 17:44:02 -0700

    fix again

--- middleware/generateTokenConsumer.js
+++ middleware/generateTokenConsumer.js
@@ -6,8 +6,8 @@ function generateToken(user) {
     // verify signature -> a secret
     // console.log(user);
     const payload = {
-        имя: user.имя,
-        потребительИД: user.потребительИД,
+        username: user.username,
+        consumerId: user.consumerId,
     };
     
     const options = {
